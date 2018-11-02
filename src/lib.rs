703cd04de09d226d4472b015edcd0b49a7198f2c
Egor Larionov <egor.larionov@gmail.com>
2018-11-01 22:13:01 -0700

    Added a test for the approximate kernel

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@ extern crate nalgebra as na;
 extern crate geometry as geo;
 extern crate hrbf;
 
+#[cfg(test)]
+#[macro_use]
+extern crate approx;
+
 use rayon::prelude::*;
 use geo::mesh::{attrib, topology::*, Attrib, PolyMesh};
 use na::{dot, Vector3};
@@ -316,10 +320,12 @@ pub fn compute_potential<F>(
     }
 }
 
+#[derive(Debug)]
 pub enum Error {
     Interrupted,
     MissingNormals,
     Failure,
+    IO(geo::io::Error),
 }
 
 impl From<attrib::Error> for Error {
@@ -328,5 +334,83 @@ impl From<attrib::Error> for Error {
             attrib::Error::TypeMismatch => Error::MissingNormals,
             _ => Error::Failure,
         }
+    }
+}
+
+impl From<geo::io::Error> for Error {
+    fn from(err: geo::io::Error) -> Self {
+        Error::IO(err)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+    use geo::io::{load_polymesh};
+
+    /// Generate a [-1,1]x[-1,1] mesh grid with the given cell resolution.
+    fn make_grid(nx: usize, ny: usize) -> PolyMesh<f64> {
+        let mut positions = Vec::new();
+
+        // iterate over vertices
+        for i in 0..nx+1 {
+            for j in 0..ny+1 {
+                positions.push([-1.0 + 2.0*(i as f64)/nx as f64, -1.0 + 2.0*(j as f64)/ny as f64, 0.0]);
+            }
+        }
+
+        let mut indices = Vec::new();
+
+        // iterate over faces
+        for i in 0..nx {
+            for j in 0..ny {
+                indices.push(4);
+                indices.push((nx+1)*j + i);
+                indices.push((nx+1)*j + i + 1);
+                indices.push((nx+1)*(j+1) + i + 1);
+                indices.push((nx+1)*(j+1) + i);
+            }
+        }
+
+        let mut mesh = PolyMesh::new(positions, &indices);
+        mesh.add_attrib::<_, VertexIndex>("potential", 0.0f32).unwrap();
+        mesh
+    }
+
+    #[test]
+    fn approximate_kernel_test() -> Result<(), Error> {
+        let mut grid = make_grid(22, 22);
+
+        let points = vec![
+            [-0.5,  0.0,  0.0],
+            [ 0.5,  0.0,  0.0],
+            [ 0.0, -0.5,  0.0],
+            [ 0.0,  0.5,  0.0],
+            [ 0.0,  0.0, -0.5],
+            [ 0.0,  0.0,  0.5],
+        ];
+
+        let normals = vec![
+            [-1.0f32,  0.0,  0.0],
+            [ 1.0,  0.0,  0.0],
+            [ 0.0, -1.0,  0.0],
+            [ 0.0,  1.0,  0.0],
+            [ 0.0,  0.0, -1.0],
+            [ 0.0,  0.0,  1.0],
+        ];
+
+        let mut sphere = PolyMesh::new(points, &vec![]);
+        sphere.add_attrib_data::<_, VertexIndex>("N", normals)?;
+        compute_potential(&mut grid, &mut sphere, Params { kernel: Kernel::Approximate { tolerance: 0.00001, radius: 1.5 } }, || false)?;
+        let solution_potential_iter = grid.attrib_iter::<f32, VertexIndex>("potential")?;
+        let expected_grid: PolyMesh<f64> = load_polymesh(&PathBuf::from("assets/approximate_sphere_test_grid_expected.vtk"))?;
+        let expected_potential_iter = expected_grid.attrib_iter::<f32, VertexIndex>("potential")?;
+
+        for (sol_pot, exp_pot) in solution_potential_iter.zip(expected_potential_iter) {
+            assert_relative_eq!(sol_pot, exp_pot, max_relative = 1e-6);
+        }
+
+        Ok(())
     }
 }

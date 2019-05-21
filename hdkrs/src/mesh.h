53513546b2b9ac5c921be7d4c840230ca19dcd40
Egor Larionov <egor.larionov@gmail.com>
2019-05-21 15:08:28 -0700

    [hdkrs] Remove static modifier from mesh fns

--- hdkrs/src/mesh.h
+++ hdkrs/src/mesh.h
@@ -11,16 +11,16 @@ namespace mesh {
 /**
  * Add the given meshes into the given detail
  */
-static void add_polymesh(GU_Detail* detail, OwnedPtr<HR_PolyMesh> polymesh);
-static void add_tetmesh(GU_Detail* detail, OwnedPtr<HR_TetMesh> tetmesh);
-static void add_pointcloud(GU_Detail* detail, OwnedPtr<HR_PointCloud> ptcloud);
-static void update_points(GU_Detail* detail, OwnedPtr<HR_PointCloud> ptcloud);
+void add_polymesh(GU_Detail* detail, OwnedPtr<HR_PolyMesh> polymesh);
+void add_tetmesh(GU_Detail* detail, OwnedPtr<HR_TetMesh> tetmesh);
+void add_pointcloud(GU_Detail* detail, OwnedPtr<HR_PointCloud> ptcloud);
+void update_points(GU_Detail* detail, OwnedPtr<HR_PointCloud> ptcloud);
 
-static OwnedPtr<HR_TetMesh> build_tetmesh(const GU_Detail *detail);
+OwnedPtr<HR_TetMesh> build_tetmesh(const GU_Detail *detail);
 
-static OwnedPtr<HR_PolyMesh> build_polymesh(const GU_Detail* detail);
+OwnedPtr<HR_PolyMesh> build_polymesh(const GU_Detail* detail);
 
-static OwnedPtr<HR_PointCloud> build_pointcloud(const GU_Detail* detail);
+OwnedPtr<HR_PointCloud> build_pointcloud(const GU_Detail* detail);
 
 } // namespace mesh
 } // namespace hdkrs

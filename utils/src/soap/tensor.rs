6af7602b7a3d9112c3b6401b8e9fde77f38f72ca
Egor Larionov <egor.larionov@gmail.com>
2019-08-17 13:06:03 -0700

    [utils] Add/Sub/Mul implementations for 1-Tensors

--- utils/src/soap/tensor.rs
+++ utils/src/soap/tensor.rs
@@ -3,6 +3,7 @@ use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
 
 /// A generic type that accepts algebraic expressions.
 #[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
+#[repr(C)]
 pub struct Tensor<T: ?Sized>(pub T);
 
 impl<T: Set> Set for Tensor<T> {
@@ -10,6 +11,13 @@ impl<T: Set> Set for Tensor<T> {
     fn len(&self) -> usize {
         self.0.len()
     }
+}
+
+/// This helper function interprets a data type `T` as a tensor. Perhaps at a
+/// later point this can be replaced by aptly designed traits for composable
+/// collection types.
+pub(crate) fn as_tensor<T>(c: &mut T) -> &mut Tensor<T> {
+    unsafe { &mut *(c as *mut T as *mut Tensor<T>) }
 }
 
 /*
@@ -152,9 +160,160 @@ impl<T: MulAssign + Copy> MulAssign<T> for Tensor<&mut [T]> {
     }
 }
 
+/*
+ * All additions and subtractions on 1-tensors represented by chunked vectors can be performed at the lowest level (flat)
+ */
+
+macro_rules! impl_chunked_tensor_arithmetic {
+    ($chunked:ident) => {
+        impl<S, O> Add for Tensor<$chunked<S, O>>
+        where
+            $chunked<S, O>: Set,
+            S: ToOwnedData,
+            Tensor<S>: Add<Output = Tensor<S::OwnedData>>,
+        {
+            type Output = Tensor<$chunked<S::OwnedData, O>>;
+            fn add(self, other: Self) -> Self::Output {
+                assert_eq!(self.0.len(), other.0.len());
+                let $chunked { chunks, data } = self.0;
+                Tensor($chunked {
+                    chunks,
+                    data: (Tensor(data) + Tensor(other.0.data)).0,
+                })
+            }
+        }
+
+        impl<S, O> Sub for Tensor<$chunked<S, O>>
+        where
+            $chunked<S, O>: Set,
+            S: ToOwnedData,
+            Tensor<S>: Sub<Output = Tensor<S::OwnedData>>,
+        {
+            type Output = Tensor<$chunked<S::OwnedData, O>>;
+
+            /// Subtract a tensor of chunked from another.
+            fn sub(self, other: Self) -> Self::Output {
+                assert_eq!(self.0.len(), other.0.len());
+                let $chunked { chunks, data } = self.0;
+                Tensor($chunked {
+                    chunks,
+                    data: (Tensor(data) - Tensor(other.0.data)).0,
+                })
+            }
+        }
+
+        /*
+         * Scalar multiplication
+         */
+
+        impl<S, O, T> Mul<T> for Tensor<$chunked<S, O>>
+        where
+            $chunked<S, O>: Set,
+            S: ToOwnedData,
+            Tensor<S>: Mul<T, Output = Tensor<S::OwnedData>>,
+        {
+            type Output = Tensor<$chunked<S::OwnedData, O>>;
+
+            fn mul(self, other: T) -> Self::Output {
+                let $chunked { chunks, data } = self.0;
+                Tensor($chunked {
+                    chunks,
+                    data: (Tensor(data) * other).0,
+                })
+            }
+        }
+
+        /*
+         * Add/Sub/Mul assign variants of the above operators.
+         */
+
+        impl<S, T, O, I> AddAssign<Tensor<$chunked<T, I>>> for Tensor<$chunked<S, O>>
+        where
+            $chunked<S, O>: Set,
+            $chunked<T, I>: Set,
+            Tensor<S>: AddAssign<Tensor<T>>,
+        {
+            fn add_assign(&mut self, other: Tensor<$chunked<T, I>>) {
+                assert_eq!(self.0.len(), other.0.len());
+                let tensor = as_tensor(&mut self.0.data);
+                *tensor += Tensor(other.0.data);
+            }
+        }
+
+        impl<S, T, O, I> SubAssign<Tensor<$chunked<T, I>>> for Tensor<$chunked<S, O>>
+        where
+            $chunked<S, O>: Set,
+            $chunked<T, I>: Set,
+            Tensor<S>: SubAssign<Tensor<T>>,
+        {
+            fn sub_assign(&mut self, other: Tensor<$chunked<T, I>>) {
+                assert_eq!(self.0.len(), other.0.len());
+                let tensor = as_tensor(&mut self.0.data);
+                *tensor -= Tensor(other.0.data);
+            }
+        }
+
+        impl<S, O, T> MulAssign<T> for Tensor<$chunked<S, O>>
+        where
+            $chunked<S, O>: Set,
+            Tensor<S>: MulAssign<T>,
+        {
+            fn mul_assign(&mut self, other: T) {
+                let tensor = as_tensor(&mut self.0.data);
+                *tensor *= other;
+            }
+        }
+    };
+}
+
+impl_chunked_tensor_arithmetic!(Chunked);
+impl_chunked_tensor_arithmetic!(UniChunked);
+
 // TODO: Figure out how to do tensor contractions.
 /*
  * Outer product.
  * Note that 1-Tensors are considered as column vectors.
  * Row vectors are `Chunked1` flat collections.
  */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn tensor_chunked() {
+        let offsets = [0, 3, 4];
+        let mut a = Chunked::from_offsets(&offsets[..], vec![1, 2, 3, 4]);
+        let b = Chunked::from_offsets(&offsets[..], vec![5, 6, 7, 8]);
+
+        // Add
+        let res = Chunked::from_offsets(&offsets[..], vec![6, 8, 10, 12]);
+        assert_eq!(Tensor(res.clone()), Tensor(a.view()) + Tensor(b.view()));
+
+        // AddAssign
+        let mut tensor_a = Tensor(a.view_mut());
+        tensor_a += Tensor(b.view());
+        assert_eq!(res.view(), a.view());
+
+        // MulAssign
+        let res = Chunked::from_offsets(&offsets[..], vec![600, 800, 1000, 1200]);
+        let mut tensor_a = Tensor(a.view_mut());
+        tensor_a *= 100;
+        assert_eq!(res.view(), a.view());
+
+        // SubAssign
+        let res = Chunked::from_offsets(&offsets[..], vec![595, 794, 993, 1192]);
+        let mut tensor_a = Tensor(a.view_mut());
+        tensor_a -= Tensor(b.view());
+        assert_eq!(res.view(), a.view());
+    }
+
+    #[test]
+    fn tensor_uni_chunked() {
+        let a = Chunked2::from_flat(vec![1, 2, 3, 4]);
+        let b = Chunked2::from_flat(vec![5, 6, 7, 8]);
+        assert_eq!(
+            Tensor(Chunked2::from_flat(vec![6, 8, 10, 12])),
+            Tensor(a.view()) + Tensor(b.view())
+        );
+    }
+}

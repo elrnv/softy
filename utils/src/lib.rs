3e266c2544464d5571489e980baf4ab9b84473a0
Egor Larionov <egor.larionov@gmail.com>
2019-11-11 10:04:05 -0800

    [utils] Added approx_power_of_two methods
    This essentially truncates the mantissa of floats which allows for
    building scaling factors that don't cause additional round-off errors.
    For instance if s is a scale then doing (x*s)/s is lossless if s has a
    zero mantissa.

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -8,7 +8,6 @@ pub mod zip;
  */
 pub use crate::zip::*;
 
-
 /// Generate a random vector of triplets.
 pub fn random_vectors(n: usize) -> Vec<[f64; 3]> {
     use rand::{distributions::Uniform, Rng, SeedableRng, StdRng};
@@ -17,4 +16,39 @@ pub fn random_vectors(n: usize) -> Vec<[f64; 3]> {
     (0..n)
         .map(move |_| [rng.sample(range), rng.sample(range), rng.sample(range)])
         .collect()
+}
+
+/// This function effectively zeros out the entire mantissa of the floating point number given.
+///
+/// The returned value is essentially the approximate max (in absolute value) power of two
+/// (including negative powers) below the given number. This works for all normal floats.
+pub fn approx_power_of_two64(f: f64) -> f64 {
+    f64::from_bits(f.to_bits() & 0xfff0_0000_0000_0000)
+}
+
+/// This function effectively zeros out the entire mantissa of the floating point number given.
+///
+/// The returned value is essentially the approximate max (in absolute value) power of two
+/// (including negative powers) below the given number. This works for all normal floats.
+pub fn approx_power_of_two32(f: f32) -> f32 {
+    f32::from_bits(f.to_bits() & 0xff800000)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn approx_power_of_two64_test() {
+        assert_eq!(approx_power_of_two64(1312.421), 1024.0);
+        assert_eq!(approx_power_of_two64(0.4231), 0.25);
+        assert_eq!(approx_power_of_two64(0.001), 0.0009765625);
+    }
+
+    #[test]
+    fn approx_power_of_two32_test() {
+        assert_eq!(approx_power_of_two32(1312.421), 1024.0);
+        assert_eq!(approx_power_of_two32(0.4231), 0.25);
+        assert_eq!(approx_power_of_two32(0.001), 0.0009765625);
+    }
 }

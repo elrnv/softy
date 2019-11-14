f5ff49c8d9e5c418b4b41847787487e7910d4ef9
Egor Larionov <egor.larionov@gmail.com>
2019-11-14 13:44:20 -0800

    [softy] Relaxed benchmark for hi res grid

--- softy/benches/box.rs
+++ softy/benches/box.rs
@@ -27,7 +27,7 @@ fn box_bench(c: &mut Criterion) {
 
     let const_volume_stretch_material = stretch_material().with_volume_preservation(true);
 
-    for i in (2..38).step_by(2) {
+    for i in (2..32).step_by(2) {
         let box_mesh = make_stretched_box(i);
 
         group.bench_function(BenchmarkId::new("Box Stretch", i), |b| {
@@ -89,10 +89,10 @@ fn box_bench(c: &mut Criterion) {
             b.iter(|| engine.step().is_ok())
         });
 
-        // 100x100 grids
-        let (top_grid, bottom_grid) = make_grids(100);
+        // 20x20 grids
+        let (top_grid, bottom_grid) = make_grids(20);
 
-        group.bench_function(BenchmarkId::new("Box Point Contact Grid 100", i), |b| {
+        group.bench_function(BenchmarkId::new("Box Point Contact Grid 20", i), |b| {
             let mut engine = SolverBuilder::new(DYNAMIC_PARAMS)
                 .add_solid(contact_box.clone(), stretch_material())
                 .add_fixed(top_grid.clone(), 1)
@@ -103,7 +103,7 @@ fn box_bench(c: &mut Criterion) {
             b.iter(|| engine.step().is_ok())
         });
 
-        group.bench_function(BenchmarkId::new("Box Linearized Point Contact Grid 100", i), |b| {
+        group.bench_function(BenchmarkId::new("Box Linearized Point Contact Grid 20", i), |b| {
             let mut engine = SolverBuilder::new(DYNAMIC_PARAMS)
                 .add_solid(contact_box.clone(), stretch_material())
                 .add_fixed(top_grid.clone(), 1)
@@ -120,7 +120,7 @@ fn box_bench(c: &mut Criterion) {
 
 criterion_group!(
     name = benches;
-    config = Criterion::default().sample_size(20);
+    config = Criterion::default().sample_size(15);
     targets = box_bench
 );
 criterion_main!(benches);

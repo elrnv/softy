47ae633a0071f199959fda690c38e1809b6f70e8
Egor Larionov <egor.larionov@gmail.com>
2019-11-15 00:39:47 -0800

    [utils] Add mesh laplacian impl
    Not currently used but could be useful later.

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -8,6 +8,66 @@ pub mod zip;
  */
 pub use crate::zip::*;
 
+#[macro_export]
+macro_rules! iter_vals {
+    // Base case
+    ($first:expr) => {
+        std::iter::once($first)
+    };
+    // Recursive case
+    ($first:expr $(,$vals:expr)*) => {
+        std::iter::once($first).chain(
+            iter_vals!($($vals,)*)
+        )
+    };
+    // Accept trailing commas
+    ($first:expr $(,$vals:expr)*,) => {
+        iter_vals!($first $(,$vals)*)
+    };
+}
+
+/// Build a matrix that smoothes values at mesh vertices with their neighbours by the given
+/// weight. For `weight = 0.0`, no smoothing is performed, and this matrix is the identity.
+pub fn build_mesh_laplacian(mesh: &geo::mesh::TriMesh<f64>, weight: f64) -> soap::DSMatrix {
+    use geo::mesh::topology::*;
+    use soap::*;
+    let size = mesh.num_vertices();
+    let iter = mesh
+        .face_iter()
+        .flat_map(|&[v0, v1, v2]| {
+            iter_vals!(
+                (v0, v1, weight),
+                (v0, v2, weight),
+                (v1, v0, weight),
+                (v1, v2, weight),
+                (v2, v0, weight),
+                (v2, v1, weight),
+            )
+        })
+        .chain((0..size).map(|i| (i, i, (1.0 - weight))));
+    let lap = DSMatrix::from_triplets_iter_uncompressed(iter, size, size);
+
+    // Compress and prune
+    // We multiply the contributions by 0.5 only when there are overlaps. From the triangle
+    // mesh topology we know that there will be at most two overlaps, and if there are none,
+    // that means the edge has only one adjacent face (a boundary edge).
+    let mut lap_data = lap
+        .data
+        .pruned(|a, &b| *a += 0.5 * (*a + b), |_, _, &val| val != 0.0);
+
+    // Normalize rows
+    for (row_idx, mut row) in lap_data.iter_mut().enumerate() {
+        let n = (row.len() - 1) as f64;
+        for (col_idx, val) in row.indexed_source_iter_mut() {
+            if row_idx != col_idx {
+                *val /= n;
+            }
+        }
+    }
+
+    Tensor::new(lap_data)
+}
+
 /// Generate a random vector of triplets.
 pub fn random_vectors(n: usize) -> Vec<[f64; 3]> {
     use rand::{distributions::Uniform, Rng, SeedableRng, StdRng};

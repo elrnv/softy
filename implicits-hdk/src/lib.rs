bc4370ebe012e4ef7f284e52628de8dc6bd8e382
Egor Larionov <egor.larionov@gmail.com>
2020-07-19 23:09:31 -0700

    [implicits-hdk] Improve interface
    Refactored the C facing api and added base radius capability.
    This allows us to construct an implicit field with an absolute radius
    rather than one relative to the size of the mesh, which is useful when
    comparing implicit fields generated by the simulation, which keeps the
    base radius constant throughout the sim.

--- implicits-hdk/src/lib.rs
+++ implicits-hdk/src/lib.rs
@@ -2,13 +2,65 @@ mod api;
 
 use hdkrs::{cffi, interop};
 
+pub use cimplicits::iso_default_params;
+pub use cimplicits::ISO_Params;
+
+/// The particular iso-surface related action to be taken.
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+pub enum HISO_Action {
+    /// Compute the implicit field at a given set of query points.
+    ComputePotential,
+    /// Project the given set of points to one side of the iso-surface.
+    Project,
+}
+
+/// A C interface for passing parameters from SOP parameters to the Rust code.
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+pub struct HISO_Params {
+    pub action: HISO_Action,
+    pub iso_value: f32,      // Only used for projection
+    pub project_below: bool, // Only used for projection
+    pub debug: bool,         // Only used for potential computation
+    pub iso_params: ISO_Params,
+}
+
+impl Default for HISO_Params {
+    fn default() -> Self {
+        HISO_Params {
+            action: HISO_Action::ComputePotential,
+            iso_value: 0.0,
+            project_below: false,
+            debug: false,
+            iso_params: ISO_Params::default(),
+        }
+    }
+}
+
+/// Construct a default set of input parameters.
+///
+/// It is safer to use this function than populating the fields manually since it guarantees that
+/// every field is properly initialized.
+#[no_mangle]
+pub unsafe extern "C" fn hiso_default_params() -> HISO_Params {
+    Default::default()
+}
+
+impl Into<implicits::Params> for HISO_Params {
+    fn into(self) -> implicits::Params {
+        self.iso_params.into()
+    }
+}
+
 /// Main entry point from Houdini SOP.
+///
 /// The purpose of this function is to cleanup the inputs for use in Rust code.
 #[no_mangle]
-pub unsafe extern "C" fn el_iso_cook(
+pub unsafe extern "C" fn hiso_cook(
     samplemesh: *mut cffi::HR_PolyMesh,
     polymesh: *mut cffi::HR_PolyMesh,
-    params: api::Params,
+    params: HISO_Params,
     interrupt_checker: *mut cffi::c_void,
     check_interrupt: Option<extern "C" fn(*const cffi::c_void) -> bool>,
 ) -> cffi::HR_CookResult {

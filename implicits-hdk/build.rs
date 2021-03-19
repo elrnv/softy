c62a1ac6e5efcf95ada668f63a6839be5ae44fd0
Egor Larionov <egor.larionov@gmail.com>
2021-03-19 11:07:00 -0700

    Fix remaining hdk build issues and inconsistencies

--- implicits-hdk/build.rs
+++ implicits-hdk/build.rs
@@ -4,26 +4,16 @@ use std::path::{Path, PathBuf};
 fn main() {
     CFG.include_prefix = "implicits";
 
-    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-
-    let target_dir = out_path
-        .parent()
-        .and_then(Path::parent)
-        .and_then(Path::parent)
-        .unwrap();
-
-    let mut build = cxx_build::bridge("src/lib.rs");
-
-    // For cimplicits.h include
-    build.include(target_dir);
-
+    let build = cxx_build::bridge("src/lib.rs");
     cmake::Config::new(".")
         .no_build_target(true)
         .init_c_cfg(build.clone())
         .init_cxx_cfg(build)
         .build();
 
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rustc-link-lib=static=cxxbridge");
-    println!("cargo:rustc-link-search=native={}", out_path.display());
+    println!("cargo:rustc-link-search=native={}", out_dir);
 }
--- implicits-hdk/hdk/ImplicitsConfig.cmake
+++ implicits-hdk/hdk/ImplicitsConfig.cmake
@@ -4,17 +4,18 @@ else()
     set( Implicits_LIB_DIR "${CMAKE_SOURCE_DIR}/../../target/release" )
 endif()
 
+find_path( cimplicits_INCLUDE_DIR cimplicits.h PATHS "${Implicits_LIB_DIR}" DOC "CImplicits include directory")
 find_path( Implicits_INCLUDE_DIR implicits/src/lib.rs.h PATHS "${CMAKE_SOURCE_DIR}/../../target/cxxbridge" DOC "Implicits include directory")
 find_library( Implicits_LIBRARY implicitshdk PATHS ${Implicits_LIB_DIR} DOC "Implicits library directory")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
     Implicits
-    REQUIRED_VARS Implicits_LIBRARY Implicits_INCLUDE_DIR
+    REQUIRED_VARS Implicits_LIBRARY Implicits_INCLUDE_DIR cimplicits_INCLUDE_DIR
     )
 
 if( Implicits_FOUND )
-    set( Implicits_INCLUDE_DIRS ${Implicits_INCLUDE_DIR} )
+    set( Implicits_INCLUDE_DIRS ${Implicits_INCLUDE_DIR} ${cimplicits_INCLUDE_DIR})
     set( Implicits_LIBRARIES ${Implicits_LIBRARY} )
 endif( Implicits_FOUND )
 
